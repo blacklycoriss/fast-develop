3750bbabf07c42a66dad3f4fbcc01508646d8b61
BlackLycoriss <prokopovaleksandr544@gmail.com>
2024-10-04 00:39:18 +0700

    add index()

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class BbsController extends Controller
 {
-    //
+    public function index()
+    {
+        return response('Здесь будет перечень объявлений.')
+            ->header('Content-Type', 'text/plain');
+    }
 }
