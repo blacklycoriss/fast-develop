453a01b43f7fa57d1454cbc6af6eb1098c10d80a
BlackLycoriss <prokopovaleksandr544@gmail.com>
2024-10-04 22:12:00 +0700

    modified index() to rendering

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -9,16 +9,8 @@ class BbsController extends Controller
 {
     public function index()
     {
-        $bbs = Bb::latest()->get();
-        $s = "Объявления\r\n\r\n";
-        foreach($bbs as $bb)
-        {
-            $s .= $bb->title . "\r\n";
-            $s .= $bb->price . " руб.\r\n";
-            $s .= "\r\n";
-        }
-        return response($s)
-            ->header('Content-Type', 'text/plain');
+        $context = ['bbs' => Bb::latest()->get()];
+        return view('index', $context);
     }
 
     public function detail(Bb $bb)
