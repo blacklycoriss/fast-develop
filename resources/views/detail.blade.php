6a0b92f8c381e3afd001e9aedd270994c78d9753
BlackLycoriss <prokopovaleksandr544@gmail.com>
2024-10-04 22:34:54 +0700

    correct detail view

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Объявления</title>
+        <title>{{ $bb->title }} :: Объявления</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
               rel="stylesheet"
               integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
@@ -14,28 +14,10 @@
     <body>
         <div class="container">
             <h1 class="my-3 text-center">Объявления</h1>
-            @if (count($bbs) > 0)
-            <table class="table table-striped table-borderless">
-                <thead>
-                    <tr>
-                        <th>Товар</th>
-                        <th>Цена, руб.</th>
-                        <th>&nbsp;</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($bbs as $bb)
-                    <tr>
-                        <td><h4>{{ $bb->title }}</h4></td>
-                        <td>{{ $bb->price }}</td>
-                        <td>
-                            <a href="/{{ $bb->id }}">Подробнее...</a>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            @endif
+            <h2>{{ $bb->title }}</h2>
+            <p>{{ $bb->content }}</p>
+            <p>{{ $bb->price }} руб.</p>
+            <p><a href="/">На перечень объявлений</a></p>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
                 integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
