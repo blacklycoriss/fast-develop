9a461247f8cac05c500cc8f3a02b6155bb6ba3ea
BlackLycoriss <prokopovaleksandr544@gmail.com>
2024-10-04 10:17:55 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 use App\Http\Controllers\BbsController;
 Route::get('/', [BbsController::class, 'index']);
+Route::get('/{bb}', [BbsController::class, 'detail']);
