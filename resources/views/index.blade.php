b445cfe752ca09478dbfcdc0ae05753fc8904f28
BlackLycoriss <prokopovaleksandr544@gmail.com>
2024-10-04 22:18:18 +0700

    create index.blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Объявления</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="my-3 text-center">Объявления</h1>
+            <table class="table table-striped table-borderless">
+                <thead>
+                    <tr>
+                        <th>Товар</th>
+                        <th>Цена, руб.</th>
+                        <th>&nbsp;</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><h4></h4></td>
+                        <td></td>
+                        <td>
+                            <a href="">Подробнее...</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
