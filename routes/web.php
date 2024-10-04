acfa0d618703e476db2ba20a1e2ced57f1fc46b4
BlackLycoriss <prokopovaleksandr544@gmail.com>
2024-10-04 22:48:53 +0700

    named routes

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 use App\Http\Controllers\BbsController;
-Route::get('/', [BbsController::class, 'index']);
-Route::get('/{bb}', [BbsController::class, 'detail']);
+Route::get('/', [BbsController::class, 'index'])->name('index');
+Route::get('/{bb}', [BbsController::class, 'detail'])->name('detail');
