e357f399278676fa17939c1662a13a79acb9b96e
BlackLycoriss <prokopovaleksandr544@gmail.com>
2024-10-04 22:46:28 +0700

    detail extends app

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,27 +1,9 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>{{ $bb->title }} :: Объявления</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-              rel="stylesheet"
-              integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-              crossorigin="anonymous">
-    </head>
-    <body>
-        <div class="container">
-            <h1 class="my-3 text-center">Объявления</h1>
-            <h2>{{ $bb->title }}</h2>
-            <p>{{ $bb->content }}</p>
-            <p>{{ $bb->price }} руб.</p>
-            <p><a href="/">На перечень объявлений</a></p>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-                integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-                crossorigin="anonymous">
-        </script>
-    </body>
-</html>
+@extends('layouts.app')
+@section('title', $bb->title)
+
+@section('content')
+    <h2>{{ $bb->title }}</h2>
+    <p>{{ $bb->content }}</p>
+    <p>{{ $bb->price }} руб.</p>
+    <p><a href="/">На перечень объявлений</a></p>
+@endsection('content')
