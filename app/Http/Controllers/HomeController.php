20f4d5d591019f17af4efd5789876868947bff03
Messmer <prokopovaleksandr544@gmail.com>
2024-10-07 17:29:08 +0700

    add update()

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -46,4 +46,14 @@ class HomeController extends Controller
     {
         return view('bb-edit', ['bb' => $bb]);
     }
+
+    public function update(Request $request, Bb $bb)
+    {
+        $bb->fill(['title' => $request->input('title'),
+            'content' => $request->input('content'),
+            'price' => $request->input('price')]);
+        $bb->save();
+
+        return redirect()->route('home');
+    }
 }
